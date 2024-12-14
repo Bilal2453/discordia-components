ca2b13cc9c40242416471fe957fd2b6947c75283
Bilal2453 <belal2453@gmail.com>
2024-12-15 01:33:01 +0300

    bump version to 1.1.0

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "Bilal2453/discordia-components",
-  version = "1.0.0",
+  version = "1.1.0",
   description = "A Discordia extension to provide Message Components support such as Buttons.",
   tags = { "discordia", "discord", "components", "buttons", "select menus" },
   license = "Apache License 2.0",
